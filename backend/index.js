4a215e1c1fd84523a2bfca003b3066a1cf50421f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-23 12:03:38 +0530

    HTTPS request

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require("express")
-const PORT = 4000;
+const PORT = 5555;
 
 
 const app = express();
+
+app.get('/' , (req,res) => {
+    // console.log(req);
+    return res.status(234).send("Welcome to HTTPs Request ! ")
+})
 
 app.listen(PORT , () => {
     console.log(`App is listening to the port : ${PORT}`)
