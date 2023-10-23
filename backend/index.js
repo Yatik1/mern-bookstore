30a26dc0091897f9e49b79a09c7ca3eba40b9fc4
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-23 12:22:09 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
-const express = require("express")
+const express = require("express");
+const mongoose  = require("mongoose");
 const PORT = 5555;
 
+const mongoUrl = 'mongodb+srv://yatiksrivastava1:AqtTkN5gOOz9Tcjw@cluster0.6nobb31.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express();
 
@@ -9,7 +11,17 @@ app.get('/' , (req,res) => {
     return res.status(234).send("Welcome to HTTPs Request ! ")
 })
 
-app.listen(PORT , () => {
+mongoose.connect(mongoUrl)
+    .then(() => {
+     console.log("database is connected !");
+     app.listen(PORT , () => {
     console.log(`App is listening to the port : ${PORT}`)
 })
+
+
+    })
+   .catch((err) => {
+    console.log(err);
+   })
+
 
