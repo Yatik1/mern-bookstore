69f4e907cd20ce3ebba7cb808941fae01ae320b5
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-23 23:18:49 +0530

    delete route

--- backend/index.js
+++ backend/index.js
@@ -100,6 +100,25 @@ app.put("/books/:id" , async (req,res) => {
     }
 })
 
+//delete data or info
+app.delete("/books/:id" , async (req,res) => {
+    try {
+        const {id} = req.params;
+        const result = await Book.findByIdAndDelete(id)
+
+        if(!result) {
+            return res.status(404).json({message : 'Book not found'})
+        }
+
+        return res.status(200).send({message : 'Book deleted successfully !'})
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message : error.message})
+    }
+})
+
+
 mongoose.connect(mongoUrl)
     .then(() => {
      console.log("database is connected !");
