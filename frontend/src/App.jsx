f19cafc31c23faa56b03d533919b44ea3e90ecbb
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-10-25 12:12:04 +0530

    ::Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Home from "../pages/Home"
+import CreateBook from "../pages/CreateBook"
+import ShowBook from "../pages/ShowBook"
+import EditBook from "../pages/DeleteBook"
+import DeleteBook from '../pages/DeleteBook'
 
 const App = () => {
   return (
-    <div className='bg-purple-700 text-white'>
-      App
-    </div>
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/books/create' element={<CreateBook />} />
+    <Route path='/books/details/:id' element={<ShowBook />} />
+    <Route path='/books/edit/:id' element={<EditBook />} />
+    <Route path='/books/delete/:id' element={<DeleteBook />} />
+  </Routes>
   )
 }
 
